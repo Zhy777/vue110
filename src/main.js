35c71c74af2583cb332a05aef0cdc3440af59a47
zhy <2282917871@qq.com>
2020-03-18 21:10:12 +0800

    init my project

--- src/main.js
+++ src/main.js
@@ -4,12 +4,27 @@ import Vue from "vue"
 //导入app
 import app from "./App.vue"
 
+//1.1导入路由
+import vueRouter from "vue-router"
+//1.2使用路由
+Vue.use(vueRouter)
+    //1.3导入自己的router.js
+import router from "./router.js"
+
 //导入mui
 import "./lib/MUI/css/mui.min.css"
+
+//导入mui font
+import "./lib/MUI/fonts/mui-icons-extra.ttf"
+
+//导入mui icons-extra
+import "./lib/MUI/css/icons-extra.css"
+
 //导入 mint-ui
 import { Header } from 'mint-ui';
 Vue.component(Header.name, Header);
 var vm = new Vue({
     el: "#app",
-    render: c => c(app)
+    render: c => c(app),
+    router //1.4挂载路由对象
 })
--- src/router.js
+++ src/router.js
@@ -1,13 +1,24 @@
 //1，导入vue-router包
 import VueRouter from "vue-router"
 
+import homeContainer from "./compontents/tabber/homeContainer.vue"
+import membeContainer from "./compontents/tabber/membeContainer.vue"
+import shoppingContainer from "./compontents/tabber/shoopingContainer.vue"
+import searchContainer from "./compontents/tabber/searchContainer.vue"
 
 
 
 
 //3，创建路由对象
 var router = new VueRouter({
-    routes: []
+    routes: [ //配置路由规则
+        { path: "/home", component: homeContainer },
+        { path: "/member", component: membeContainer },
+        { path: "/shopping", component: shoppingContainer },
+        { path: "/search", component: searchContainer }
+
+    ],
+    linkActiveClass: "mui-active" //设置默认高亮的类
 })
 
 export default router
